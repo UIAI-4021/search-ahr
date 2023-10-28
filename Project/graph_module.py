a28531e5d1b664afac9065e42561169fccc3d2cb
hhaa1382 <mehranfarhamid82@gmail.com>
2023-10-28 20:15:25 +0330

    graph class add

--- Project/graph_module.py
+++ Project/graph_module.py
@@ -5,3 +5,26 @@ class Node:
 
     def add_neighbor(self, node, edge):
         self.neighbors[node] = edge
+
+
+class Graph:
+    def __init__(self):
+        self.vertices_num = 0
+        self.vertices = []
+
+    def check_vertex(self, name):
+        for node in self.vertices:
+            if node.name == name:
+                return node
+
+        self.vertices_num += 1
+        node = Node(name)
+        self.vertices.append(node)
+        return node
+
+    def add_edge(self, edge, name1, name2):
+        node1 = self.check_vertex(name1)
+        node2 = self.check_vertex(name2)
+
+        node1.add_neighbor(node2, edge)
+        node2.add_neighbor(node1, edge)
