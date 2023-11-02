943397e7ef65a0445c4eea6e2db0e69357e779fa
hhaa1382 <mehranfarhamid82@gmail.com>
2023-11-02 17:00:54 +0330

    graph class changed

--- Project/graph_module.py
+++ Project/graph_module.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, data):
+        self.name = data
         self.neighbors = {}
 
     def add_neighbor(self, node, edge):
@@ -12,19 +12,25 @@ class Graph:
         self.vertices_num = 0
         self.vertices = []
 
-    def check_vertex(self, name):
+    def check_vertex(self, data):
         for node in self.vertices:
-            if node.name == name:
+            if checkValues(node.data, data):
                 return node
 
         self.vertices_num += 1
-        node = Node(name)
+        node = Node(data)
         self.vertices.append(node)
         return node
 
-    def add_edge(self, edge, name1, name2):
-        node1 = self.check_vertex(name1)
-        node2 = self.check_vertex(name2)
+    def add_edge(self, edge, source_data, destination_data):
+        node1 = self.check_vertex(source_data)
+        node2 = self.check_vertex(destination_data)
 
         node1.add_neighbor(node2, edge)
-        node2.add_neighbor(node1, edge)
+
+
+def checkValues(data1, data2):
+    if data1.airport == data2.airport and data1.city == data2.city and data1.latitude == data2.latitude \
+            and data1.longitude == data2.longitude and data1.country == data2.country:
+        return True
+    return False
