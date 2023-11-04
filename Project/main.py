39c8c10a9e1e2f708495e03b7aeda6e12a720067
hhaa1382 <mehranfarhamid82@gmail.com>
2023-11-05 00:22:38 +0330

    time calculation add

--- Project/main.py
+++ Project/main.py
@@ -2,13 +2,16 @@ from graph_module import *
 from a_star_module import *
 from dijkstra_module import *
 from data_module import *
+from time_module import *
 import pandas as pd
 
 
-def readData(input_values):
+graph = Graph()
+dijkstra=Dijkstra(graph)
+
+
+def readData():
     df = pd.read_csv("Flight_Data.csv", encoding="latin-1", on_bad_lines="skip")
-    graph = Graph()
-    dijkstra=Dijkstra(graph)
     size = len(df.SourceAirport_City)
     for i in range(0, size):
         source_data = Data(df.SourceAirport[i], df.SourceAirport_City[i], df.SourceAirport_Latitude[i],
@@ -22,11 +25,19 @@ def readData(input_values):
 
         graph.add_edge(edge, source_data, destination_data)
 
-    starting_airport, ending_airport = input_values.split(" - ")
-    a_star_list=a_star_search(graph,starting_airport,ending_airport)
-    dijkstra_list=dijkstra.find_shortest_path(starting_airport,ending_airport)
-
 
 if __name__ == '__main__':
-    readData("Imam Khomeini International Airport - Raleigh Durham International Airport")
+    readData()
+    # input_values=input("Enter source and destination : ")
+    input_values="Imam Khomeini International Airport - Raleigh Durham International Airport"
+    starting_airport, ending_airport = input_values.split(" - ")
 
+    a_star_time=Time()
+    a_star_time.Time_starting_()
+    a_star_list = a_star_search(graph, starting_airport, ending_airport)
+    a_star_time.Time_Ending_()
+
+    dijkstra_time = Time()
+    dijkstra_time.Time_starting_()
+    dijkstra_list = dijkstra.find_shortest_path(starting_airport, ending_airport)
+    dijkstra_time.Time_Ending_()
