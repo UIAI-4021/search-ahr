b16fc18def09dc267d861fc9dcb60044368becb9
hhaa1382 <mehranfarhamid82@gmail.com>
2023-11-05 00:07:01 +0330

    read data function add

--- Project/main.py
+++ Project/main.py
@@ -7,3 +7,21 @@ import pandas as pd
 
 if __name__ == '__main__':
     pass
+
+
+def readData(input_values):
+    df = pd.read_csv("Flight_Data.csv", encoding="latin-1", on_bad_lines="skip")
+    g = Graph()
+    size = len(df.SourceAirport_City)
+    for i in range(0, size):
+        source_data = Data(df.SourceAirport[i], df.SourceAirport_City[i], df.SourceAirport_Latitude[i],
+                           df.SourceAirport_Longitude[i], df.SourceAirport_Country[i])
+
+        destination_data = Data(df.DestinationAirport[i], df.DestinationAirport_City[i],
+                                df.DestinationAirport_Latitude[i], df.DestinationAirport_Longitude[i],
+                                df.DestinationAirport_Country[i])
+
+        edge = Edge(df.Distance[i], df.FlyTime[i], df.Price[i])
+
+        g.add_edge(edge, source_data, destination_data)
+
